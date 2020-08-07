e3c8af7386ad18d15b559f01f6a58ff53ba01986
Arka Saha <40143090+ArkaJU@users.noreply.github.com>
2020-08-07 19:37:32 +0530

    Update __init__.py

--- scripts/data/__init__.py
+++ scripts/data/__init__.py
@@ -1,6 +1,7 @@
-from scripts.data import image, caltech_ucsd_birds
+from scripts.data import image, caltech_ucsd_birds, thermal_images
 
 dataset_dict = {
     'image': image.ImageDataset,
-    'ucb':  caltech_ucsd_birds.UCBDataset
+    'ucb':  caltech_ucsd_birds.UCBDataset,
+    'custom': thermal_images.CustomDataset
 }
